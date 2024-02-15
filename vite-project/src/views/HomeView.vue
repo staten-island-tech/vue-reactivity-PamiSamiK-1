937c7645182f3bca307d1a7d7189930a73c3db85
PamiSamiK <144476939+PamiSamiK@users.noreply.github.com>
2024-02-15 13:29:34 -0500

    eytq

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,9 +1,22 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    const houses=[
+      {adress:"67 Nicole Loop, Staten Island, NY 10301",
+      price:" $1,286,800",
+      bedrooms:"4",
+      bath:"4",
+      sqft:"4,400 sqft",
+   }
+    ]
+  }
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style lang="scss" scoped>
+
+</style>
